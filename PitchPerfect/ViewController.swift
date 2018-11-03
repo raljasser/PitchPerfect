a0a1264c46562c00a9658d0c389e52ae2dae1185
Rana Omar <Rana@ranas-macbook-pro-2.local>
2018-11-03 18:39:53 +0300

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Rana Omar on 25/02/1440 AH.
+//  Copyright © 1440 Rana Omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PitchPerfectTests/PitchPerfectTests.swift
@@ -0,0 +1,34 @@
+//
+//  PitchPerfectTests.swift
+//  PitchPerfectTests
+//
+//  Created by Rana Omar on 25/02/1440 AH.
+//  Copyright © 1440 Rana Omar. All rights reserved.
+//
+
+import XCTest
+@testable import PitchPerfect
+
+class PitchPerfectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
